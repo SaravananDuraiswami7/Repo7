c6a73307443cf2a41183ee063e23bda14dedfe06
Saravanan Duraiswami <47965036+SaravananDuraiswami7@users.noreply.github.com>
2019-06-24 23:33:10 +0530

    Update DemoA.java

--- GitHub/src/DemoA.java
+++ GitHub/src/DemoA.java
@@ -2,8 +2,10 @@
 public class DemoA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello Sherin");
+		System.out.println("Hello Don");
+		System.out.println("Hello Mike");
 	}
 
 }
